a9caca218d1f8a3f31f4f68f9b9f67a46b65d42e
patni11 <shubhpatni2002@gmail.com>
2025-06-25 16:32:08 -0700

    added shadcn ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NextAuthSessionProvider from "@/components/SessionProvider";
-
+import { Toaster } from "@/components/ui/sonner"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,6 +31,7 @@ export default function RootLayout({
         <NextAuthSessionProvider>
           {children}
         </NextAuthSessionProvider>
+        <Toaster />
       </body>
     </html>
   );
